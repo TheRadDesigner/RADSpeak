a449339e0021ffda49e9c5cebb9feea3c6aa4b60
Luke Stewart <hello@lukestewart.com.au>
2020-06-30 11:17:47 +1000

    CSS Reset, Libs added. Template content removed

--- header.php
+++ header.php
@@ -3,20 +3,19 @@
 
     <head>
         <meta charset="utf-8">
-        <title>My Profile Website</title>
+        <title>RAD Speak</title>
+        <!-- CSS -->
         <link rel="stylesheet" href="style.css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100;0,900;1,100&family=Palanquin+Dark&display=swap" rel="stylesheet">
+        <!-- Bootstrap --> 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script> 
+        <!-- JQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <!-- Popper.js -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     </head>
     
     <body>
     
-    <div id="container">
-            <img class="logo" src="logo.png" alt="My Profile Logo" />        
-        
-            <div id="nav">
-            <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="profile.php">My Profile</a></li>
-            </ul>
-        </div>
-            
-        <div id="content">
